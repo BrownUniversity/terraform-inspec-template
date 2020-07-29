4f3b7886464ce71b12f0dc1488fe381fab8e1431
Isabel Restrepo <isabelrestre@gmail.com>
2020-07-29 11:00:52 -0400

    chore: improve docs and formatting according to PR review

--- test/integration/simple-template/controls/project.rb
+++ test/integration/simple-template/controls/project.rb
@@ -1,5 +1,7 @@
 title "Test simple template"
 
+# Terraform outputs defined in examples/*/outputs.tf
+# become directly available to Ispec tests as attributes
 message = attribute("message")
 
 describe echo(message) do
--- test/integration/simple-template/libraries/echo.rb
+++ test/integration/simple-template/libraries/echo.rb
@@ -1,4 +1,8 @@
-
+# Inspec custom resource. 
+# Custom resources These become available with their respective names 
+# and provide easy functionality to profiles. 
+# This example shows calling a local command and capturing its stdout
+# More about custom see https://docs.chef.io/inspec/dsl_resource/
 class Echo < Inspec.resource(1)
     name 'echo'
     desc 'Validate printing a message'
